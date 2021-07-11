d8844812775d0d1748cb6a3d92abaee06d961372
Ishtrb <ishiitorben@gmail.com>
2021-07-11 18:30:58 -0300

    :sparkles: Finish the BoardGame object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './src/styles/settings/colors.css';
-import './src/styles/generic/reset.css';
-import './src/styles/elements/base.css';
-import BoardGame from './src/objects/BoardGame';
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css";
+import "./src/styles/elements/base.css";
+import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector("#root");
 const $htmlBoardGame = BoardGame(6);
